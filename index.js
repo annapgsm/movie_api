78daa54920063b1b6bdc1148c4ecbf082ee4bbfb
annapgsm <anna.pingsmann@gmail.com>
2025-09-23 17:13:55 +0200

    Updated index.js
    Imported cors and created a list of allowed domains within the variable allowedOrigins
    Updated app.post('/users') to hash password before storing it

--- index.js
+++ index.js
@@ -23,12 +23,27 @@ mongoose.connect('mongodb://localhost:27017/movieDB',
     useUnifiedTopology: true 
   });
 
+const cors = require('cors');
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application does not allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 //Middleware
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(express.static('public'));
 
 //Create
+/* 
 app.post('/users', (req, res) => {
   Users.findOne({ Username: req.params.Username})
     .then((user) => {
@@ -53,6 +68,50 @@ app.post('/users', (req, res) => {
     .catch((err) => {
       console.error(err);
       res.status(500).send('Error: ' + err);
+    });
+}); 
+*/
+
+app.post('/users', async (req, res) => {
+  //Validation Logic
+  [
+    check('Username', 'Username is required').isLength({min: 5}),
+    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail()
+  ], async (req, res) => {
+
+  // check the validation object for errors
+  let errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  let hashedPassword = Users.hashPassword(req.body.Password);
+  await Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
+    .then((user) => {
+      if (user) {
+      //If the user is found, send a response that it already exists
+        return res.status(400).send(req.body.Username + ' already exists');
+      } else {
+        Users
+          .create({
+            Username: req.body.Username,
+            Password: hashedPassword,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+          .then((user) => { res.status(201).json(user) })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+          });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
     });
 });
 
@@ -244,6 +303,7 @@ app.use((err, req, res, next) => {
 });
 
 //Start Server
-app.listen(8080, () => {
-  console.log('Server is running on port 8080');
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+ console.log('Listening on Port ' + port);
 });
