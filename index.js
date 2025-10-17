018d406c6c18d2eac4a21503dbbff2fa89cdb9e8
annapgsm <anna.pingsmann@gmail.com>
2025-10-17 13:19:03 +0200

    Updated index.html
    updated allowedOrigins

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ mongoose.connect( process.env.CONNECTION_URI,
 );
 
 
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:3000'];
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234'];
 
 app.use(cors({
   origin: (origin, callback) => {
